677224ea89d647d6a5c183b75b9c7b0d7420154a
Eduard <edik.moskalets@gmail.com>
2024-09-01 09:48:14 +0300

    contactOps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ContactList from './ContactList/ContactList';
+import css from './App.module.css';
+import SearchBox from './SearchBox/SearchBox';
+import ContactForm from './ContactForm/ContactForm';
+import { fetchContacts } from '../redux/contactsOps';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.contacts.loading);
+  const isError = useSelector(state => state.contacts.error);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      <ContactList />
+    </div>
+  );
 }
-
-export default App
